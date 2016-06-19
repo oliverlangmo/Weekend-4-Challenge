f8cbffb8b7d911725ed9408bc197d20dbc9b5dff
oliverlangmo <oliver.langmo@gmail.com>
2016-06-19 15:23:34 -0500

    with some css

--- public/scripts/FE-app.js
+++ public/scripts/FE-app.js
@@ -14,6 +14,60 @@ $('#sendTask').on('click', function(){
     url: '/addNew',
     data: newTask
    });
-
+   getTasks();
   });
+  $( '#getTasks' ).on( 'click', getTasks);
+  $( '#taskDiv' ).on( 'click','.deleteMe', deleteTask );
+  $('#taskDiv').on('click', '.completeMe', completeTask);
+
+  function deleteTask(){
+   var deleteID = $( this ).data( 'id' );
+   console.log(deleteID);
+   var delTask = {
+     "id": deleteID
+   };
+  $.ajax({
+       type: 'POST',
+       url: '/postDelete',
+       data: delTask
+   });
+   getTasks();
+   }
+
+function getTasks(){
+$.ajax({
+type : 'GET',
+url: '/getTasks',
+success: function(data) {
+  showTasks(data);
+}
+});
+}
+function completeTask() {
+  var completeID = $(this).data('id');
+  var complTask = {
+    "id": completeID
+  };
+ $.ajax({
+      type: 'POST',
+      url: '/completeTask',
+      data: complTask
+  });
+  getTasks();
+}
+$('.completeMe').on('click', function(){
+  $(this).parent().css({'background-color': 'green'});
+});
+
+function showTasks( tasks ){
+  console.log( "in showTasks: ", tasks );
+  // empty output div and input field
+  $( '#taskIn' ).val( "" );
+  $( '#taskDiv' ).empty();
+  // append each row to task div
+  for( var i=0; i<tasks.length; i++ ){
+  console.log(tasks);
+      $( '#taskDiv' ).append( '<p id = taskPane><b>Task: ' + tasks[i].entry + '<br/>Complete?: ' + tasks[i].completed + ", created: " + tasks[ i ].created +'</b><button class="completeMe" data-id="' + tasks[i].id + '">Complete</button><button class="deleteMe" data-id="' + tasks[i].id + '">Delete</button>'+'</p>' );
+  }
+}
 });
--- server/app.js
+++ server/app.js
@@ -21,6 +21,44 @@ app.get( '/', function( req, res ){
 app.post('/addNew', urlencodedParser, function(req, res){
   console.log( 'in POST addNew: ' + req.body.task + " " + req.body.complete );
     pg.connect( connectionString, function( err, client, done ){
-      client.query( 'INSERT INTO harry_tasker ( entry, completed,created  ) VALUES ( $1, $2, $3)', [ req.body.task, req.body.complete, 'now()' ] );
+      client.query( 'INSERT INTO harry_tasker ( entry, completed,created  ) VALUES ( $1, $2, $3)', [ req.body.task, req.body.complete, 'NOW()' ] );
+        done();
 });
+res.end();
+});
+app.get('/getTasks', urlencodedParser, function(req,res){
+  var results = [];
+pg.connect(connectionString, function(err, client, done){
+  var query = client.query('SELECT * from harry_tasker ORDER BY id ASC');
+  query.on( 'row', function( row ) {
+            results.push( row );
+  });
+  query.on('end', function() {
+            done();
+            // rend back results as a json
+            return res.json(results);
+        }); // end onEnd
+        if(err) {
+            console.log(err);
+        } // end error
+});
+
+});
+app.post( '/postDelete', urlencodedParser, function(req, res){
+  console.log('in postDelete: ' + req.body.id);
+  var thisID = req.body.id;
+  pg.connect( connectionString, function( err, client, done ) {
+    client.query( "DELETE FROM harry_tasker WHERE id="+ thisID);
+    done();
+  });//end app.post
+  res.end();
+});
+app.post( '/completeTask', urlencodedParser, function(req, res){
+  console.log('in completeTask: ' + req.body.id);
+  var thisID = req.body.id;
+  pg.connect( connectionString, function( err, client, done ) {
+    client.query( "UPDATE harry_tasker SET completed = 'true' WHERE id="+ thisID);
+    done();
+  });//end app.post
+  res.end();
 });
